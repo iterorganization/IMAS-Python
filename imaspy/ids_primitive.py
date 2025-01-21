fd6ac2ea7b859727a3d1e3e3b2ffa6f7b2086d78
Anushan Fernando <anushanf@google.com>
2025-01-21 10:57:10 +0000

    Formatting.

--- imaspy/ids_primitive.py
+++ imaspy/ids_primitive.py
@@ -481,7 +481,10 @@ class IDSNumericArray(IDSPrimitive, np.lib.mixins.NDArrayOperatorsMixin):
         value = np.asanyarray(value)
         if value.dtype != dtype:
             logger.info(_CONVERT_MSG, value.dtype, self)
-        value = np.asarray(value, dtype=dtype,)
+        value = np.asarray(
+            value,
+            dtype=dtype,
+        )
         if value.ndim != self.metadata.ndim:
             raise ValueError(f"Trying to assign a {value.ndim}D value to {self!r}.")
         return value
